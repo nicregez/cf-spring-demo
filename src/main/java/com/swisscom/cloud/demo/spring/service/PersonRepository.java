0f084a0a7ecd098b21db9c82ad33f6e9c1f500da
herwes <herbert.wespi@gmx.ch>
2014-10-20 10:56:12 +0200

    Simplified JPA Repository

--- src/main/java/com/swisscom/cloud/demo/spring/service/PersonRepository.java
+++ src/main/java/com/swisscom/cloud/demo/spring/service/PersonRepository.java
@@ -4,9 +4,7 @@
  */
 package com.swisscom.cloud.demo.spring.service;
 
-import java.util.List;
-
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.swisscom.cloud.demo.spring.model.Person;
 
@@ -14,14 +12,6 @@ import com.swisscom.cloud.demo.spring.model.Person;
  * @author Nicolas Regez
  * @since 24.02.2014
  */
-public interface PersonRepository extends Repository<Person, String> {
-
-    List<Person> findAll();
-
-    Person findOne(String id);
-
-    <S extends Person> S save(Person person);
-
-    void delete(Person entity);
+public interface PersonRepository extends JpaRepository<Person, String> {
 
 }
